acc38c7fb2d78de94e65619cfadeaa655865e596
VIVEKSUREKA <53266779+VIVEKSUREKA@users.noreply.github.com>
2023-05-13 12:40:23 +0530

    added google custom search api call, modified mongodb docs

--- server/controller/index.js
+++ server/controller/index.js
@@ -3,8 +3,26 @@ import dotenv from 'dotenv';
 import dishrecipe from '../modal/model.js';
 dotenv.config();
 
+// function to call google custom search api
+const search = async (query) => {
+  const config = {
+    method: 'get',
+    url: `https://customsearch.googleapis.com/customsearch/v1?cx=b4233c0ed513d408c&gl=in&num=1&q=${query}&safe=high&searchType=image&imgSize=MEDIUM&key=${process.env.GCSE_API_KEY}`,
+    headers: { }
+  };
+
+  try {
+    const response = await axios.request(config);
+    console.log(JSON.stringify(response.data),"[response from Google Search axios call]"); 
+    const imgLink = response.data.items[0].link;
+    return imgLink;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 const handleRoute = async (req, res) => {
-    const apiKey = process.env.OPENAI_API_KEY;
+    const apiKeyGPT = process.env.OPENAI_API_KEY;
     const { dish } = req.body;
     const prompt = `Please provide a recipe for ${dish}`;
     console.log(prompt);
@@ -23,83 +41,47 @@ const handleRoute = async (req, res) => {
         ]
     });
 
-    const config = {
+    const GPTconfig = {
         method: 'post',
         url: 'https://api.openai.com/v1/chat/completions',
         headers: { 
-        'Authorization': `Bearer ${apiKey}`, 
+        'Authorization': `Bearer ${apiKeyGPT}`, 
         'Content-Type': 'application/json'
         },
         data : data
     };
-    
     
 
     try {
 
         const existingRecipe = await dishrecipe.findOne({dish: `Please provide a recipe for ${dish}`});
 
-        if(existingRecipe === null){ // recipe not found
+        if(existingRecipe === null){ // recipe not found in db
 
-            const response = await axios(config);
+            const [response, imgLink] = await Promise.all([axios(GPTconfig), search(dish)]);
             const recipe = response.data.choices[0].message.content;
             const recipeForDb = await dishrecipe.create({
                 dish : prompt,
                 recipe,
+                imgLink,
             });
-            console.log(JSON.stringify(response.data),"[response from axios call]");                
+            console.log(JSON.stringify(response.data),"[response from GPT axios call]");                
             console.log({recipeForDb},"[saved to mongodb]");
 
-            res.json({recipe});
+            res.json({recipe, imgLink});
         
         }else{
 
             console.log({existingRecipe}, "[existing recipe from mongodb]");
-            res.json({recipe: existingRecipe.recipe});
+            res.json({recipe: existingRecipe.recipe, imgLink: existingRecipe.imgLink});
 
         }
 
     } catch (e) {
 
         console.log({error: e.message},"[error in try catch] ");
-        res.json("Something went wrong");
+        res.json({recipe: "Something went wrong", imgLink: ""});
     }
-    
-    
-    /*dishrecipe.findOne({dish: `Please provide a recipe for ${dish}`})
-    .then(function (response){
-
-        if(response !== null){
-            console.log({response},"[found the recipe]");
-            res.json({recipe: response.recipe});
-            return ;
-        }else {
-            axios(config)
-            .then(function (response) {
-                const recipe = response.data.choices[0].message.content;
-                const result =  dishrecipe.create({
-                    dish : prompt,
-                    recipe,
-                    createdAt: new Date(),
-                });
-
-                console.log(JSON.stringify(response.data));
-                res.json({recipe});
-                return result;
-            })
-            .then(function (result){
-                console.log({result},"[mongo db result]");
-            }).catch(function (e){
-                console.log({error: e.message} , "[ error in axios function]");
-                return e;
-            })
-        }
-        
-    })
-    .catch(function (error) {
-        console.log(error,"[error in api]");
-    });*/
-
-};
+}
 
 export default handleRoute;
